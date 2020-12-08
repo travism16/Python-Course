c5c2483ae46b29d33032767cebf4d053cc00c98e
Travis Mauthe <travismauthe@gmail.com>
2020-12-07 17:03:46 -0800

    Juniper device connection list

---
+++ Class_8/juniper_devices.py
@@ -0,0 +1,10 @@
+from getpass import getpass
+
+srx2 = {
+    "host": "srx2.lasthop.io",
+    "user": "pyclass",
+    "password": getpass()
+}
+
+# List for if multiple devices are present
+juniper_devices = ['srx2']
