3615ca49c5a21060d6c1a7ec6a08c01605fa1243
Travis Mauthe <travismauthe@gmail.com>
2020-12-07 18:57:55 -0800

    PyEZ config exercises

---
+++ Class_8/exercise3.py
@@ -0,0 +1,34 @@
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import LockError
+from pprint import pprint
+from juniper_devices import srx2
+
+my_device = Device(**srx2)
+my_device.open()
+my_device.timeout = 60
+
+cfg = Config(my_device)
+
+# Exercise 3a
+cfg.lock()
+
+try:
+    cfg.lock()
+except LockError:
+    print("Config already locked!")
+
+# Exercise 3b
+cfg.load("set system host-name Router-Man-5000", format="set", merge=True)
+
+# Exercise 3c
+print()
+print(cfg.diff())
+print()
+
+# Exercise 3d
+cfg.rollback(0)
+print("Rolling back config...")
+print()
+print(cfg.diff())
+print()
