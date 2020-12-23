d09be02bc3662a91870137e6632f9e8322c06745
Travis Mauthe <travismauthe@gmail.com>
2020-12-23 15:16:25 -0800

    device connection info file

---
+++ Class_10/my_devices.py
@@ -0,0 +1,37 @@
+from getpass import getpass
+
+
+username = "pyclass"
+password = getpass()
+
+cisco3 = {
+    "host": "cisco3.lasthop.io",
+    "username": username,
+    "password": password,
+    "device_type": "cisco_ios",
+}
+
+arista1 = {
+    "host": "arista1.lasthop.io",
+    "username": username,
+    "password": password,
+    "device_type": "arista_eos",
+    "global_delay_factor": 4,
+}
+
+arista2 = {
+    "host": "arista2.lasthop.io",
+    "username": username,
+    "password": password,
+    "device_type": "arista_eos",
+    "global_delay_factor": 4,
+}
+
+srx2 = {
+    "host": "srx2.lasthop.io",
+    "username": username,
+    "password": password,
+    "device_type": "juniper_junos",
+}
+
+devices_list = [cisco3, arista1, arista2, srx2]
