d1dd939d035a5d2d68772a16e24a07de8b359c40
Travis Mauthe <travismauthe@gmail.com>
2020-12-23 15:14:29 -0800

    Concurrent Futures ThreadPoolExecutor exercise

---
+++ Class_10/exercise3a.py
@@ -0,0 +1,26 @@
+from my_functions import ssh_command2
+from my_devices import devices_list
+from concurrent.futures import ThreadPoolExecutor, wait
+from datetime import datetime
+
+start_time = datetime.now()
+max_threads = 5
+pool = ThreadPoolExecutor(max_threads)
+
+future_list = []
+
+for device in devices_list:
+    future = pool.submit(ssh_command2, device, "show version")
+    future_list.append(future)
+
+wait(future_list)
+
+for future in future_list:
+    print("=" * 40)
+    print(future.result())
+
+end_time = datetime.now()
+print("=" * 40)
+print("\nElapsed time: ", end_time - start_time)
+print()
+
